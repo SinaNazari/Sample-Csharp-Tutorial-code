132c75f8bc137c03a2d944c4d8f53e9ad1403d3a
Sina Nazari <52839857+SinaNazari@users.noreply.github.com>
2021-09-01 07:53:33 +0430

    Update Or Create New Source

---
+++ GenericList-Project/Introduction_GenericList/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Introduction_GenericList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //List
+
+            //int[] number = { 1, 2, 3, 4, 5 };
+
+            //string[] names = { "Sina", "Ali", "Reza", "Mina" };
+
+            //List<int> number = new List<int>();
+
+
+            //GenericList
+
+            List<string> names = new List<string>();
+            names.Add("Sina");
+            names.Add("Sara");
+            names.Add("Mina");
+            names.Add("Ali");
+
+            names.Remove("Sara");
+            names.RemoveAt(3);
+
+
+
+            foreach (var item in names)
+            {
+                Console.WriteLine($"\n {item}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
