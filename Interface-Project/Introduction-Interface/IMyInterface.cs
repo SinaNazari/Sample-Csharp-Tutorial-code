fc6bd7c1ebc2a9766d009978b4b86efb3510255c
Sina Nazari <52839857+SinaNazari@users.noreply.github.com>
2021-09-02 23:20:07 +0430

    Update Or Create New Source Code

---
+++ Interface-Project/Introduction-Interface/IMyInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction_Interface
+{
+    class IMyInterface
+    {
+        string name;
+        string family;
+    }
+}
---
+++ Interface-Project/Introduction-Interface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Introduction_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
