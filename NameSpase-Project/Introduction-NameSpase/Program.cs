470ce8822bb1788978d387999b1d0a863b7c4183
Sina Nazari <52839857+SinaNazari@users.noreply.github.com>
2021-09-03 11:35:59 +0430

    Update Or Create New Source Code

---
+++ NameSpase-Project/Introduction-NameSpase/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MyAssembly;
+namespace Introduction_NameSpase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.PersianTime());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NameSpase-Project/MyAssembly/ExtentionMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAssembly
+{
+    public static class ExtentionMethod
+    {
+        public static string PersianTime(this DateTime value)
+        {
+            PersianCalendar persian = new PersianCalendar();
+            string pc = persian.GetYear(value) + "/" + persian.GetMonth(value) + "/" +
+            persian.GetDayOfMonth(value) + " " + persian.GetHour(value) + ":" + persian.GetMinute(value) +
+            ":" + persian.GetSecond(value);
+            return pc;
+        }
+    }
+}
