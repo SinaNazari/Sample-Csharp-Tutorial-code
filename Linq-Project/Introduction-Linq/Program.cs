e6c17ae5e7db35fc1f052164047f80154c6c38c3
Sina Nazari <52839857+SinaNazari@users.noreply.github.com>
2021-09-04 22:55:15 +0430

    Update Or Create New Source Code

---
+++ Linq-Project/Introduction-Linq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Introduction_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] number = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 89, 34, 234, 100, 78, 99 };
+
+
+            int[] resulat = (from n in number select n).ToArray();
+
+
+            int[] resulat2 = (from n in number orderby n descending select n).ToArray();
+
+
+        }
+    }
+}
