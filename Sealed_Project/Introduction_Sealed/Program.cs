93d99c7a0ee91b5dfe5a925df4050d288e19e442
Sina Nazari <sinanazari3000@gmail.com>
2021-10-01 12:59:12 +0330

    Update Or Create New Source Code

---
+++ Sealed_Project/Introduction_Sealed/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Introduction_Sealed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SealedCalss sealedCalss = new SealedCalss();
+            int Total = sealedCalss.Add(5, 9);
+            Console.WriteLine("Total is:" + Total.ToString());
+            Console.ReadKey();
+        }
+    }
+
+    sealed class SealedCalss
+    {
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
