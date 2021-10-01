210ae4d1e2f486d86011b092d5ef0b3c175dc869
SinaNazari <sinanazari3000@gmail.com>
2021-10-01 09:54:12 +0330

    Update Or Create new Source Code

---
+++ Abstract_Project/Introduction_Abstract/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Introduction_Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello hi = new SayHello();
+            Console.WriteLine(hi.sayHello("Sina"));
+            Console.ReadKey();
+        }
+    }
+
+    
+    public abstract class Hello
+    {
+        public virtual string sayHello(string name)
+        {
+            return "Hi";
+        }
+    }
+
+    public class SayHello : Hello
+    {
+        public override string sayHello(string name)
+        {
+            return base.sayHello("Hello"+name);
+        }
+    }
+}
